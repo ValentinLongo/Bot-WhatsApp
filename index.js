e4bbdb0112aadfce67892063aacbc5fe8a78b5dd
Valentin <longovalentin@gmail.com>
2023-10-30 20:39:37 -0300

    Desarrollo por dni

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ db.connect()
         console.log('Error al conectar a la base de datos: ', err);
     });
 
-const regex = /^Pedido (\d+)$/;
-
 client.on('qr', qr => {
     qrcode.generate(qr, { small: true });
 });
@@ -33,23 +31,22 @@ client.on('ready', () => {
 
 client.on('message', async (message) => {
     console.log(message.body);
-    const match = message.body.match(regex);
+    const match = message.body.match(/^Pedido (\d+)$/);
+    const matchDocumento = message.body.match(/^Documento (\d+)$/);
 
     if (match) {
         const numeroPedido = match[1];
-
+        // Realizar el SELECT con Pedido
         try {
             const result = await db.query(
                 `SELECT est_descri FROM tesis.Pedido
                 LEFT JOIN tesis.Estado ON ped_estado = est_codigo
+                LEFT JOIN tesis.Usuario ON ped_usuario = usu_codigo
                 WHERE ped_codigo = ${numeroPedido};`
             );
-
             if (result.length > 0) {
                 const estadoPedido = JSON.stringify(result[0][0]["est_descri"]);
-                console.log(estadoPedido);
-                const responseMessage = `Estado pedido: ${estadoPedido}`;                
-                client.sendMessage(message.from, responseMessage);
+                client.sendMessage(message.from, `Estado del pedido: ${estadoPedido}`);
             } else {
                 client.sendMessage(message.from, "No se encontró el pedido especificado.");
             }
@@ -57,8 +54,28 @@ client.on('message', async (message) => {
             console.error('Error al consultar la base de datos:', error);
             client.sendMessage(message.from, "Ocurrió un error al consultar la base de datos.");
         }
+    } else if (matchDocumento) {
+        const numeroDocumento = matchDocumento[1];
+        // Realizar el SELECT con Documento
+        try {
+            const result = await db.query(
+                `SELECT est_descri FROM tesis.Pedido
+                LEFT JOIN tesis.Estado ON ped_estado = est_codigo
+                LEFT JOIN tesis.Usuario ON ped_usuario = usu_codigo
+                WHERE usu_dni = ${numeroDocumento};`
+            );
+            if (result.length > 0) {
+                const estadoPedido = JSON.stringify(result[0][0]["est_descri"]);
+                client.sendMessage(message.from, `Estado del pedido: ${estadoPedido}`);
+            } else {
+                client.sendMessage(message.from, "No se encontró el documento especificado.");
+            }
+        } catch (error) {
+            console.error('Error al consultar la base de datos:', error);
+            client.sendMessage(message.from, "Ocurrió un error al consultar la base de datos.");
+        }
     } else {
-        client.sendMessage(message.from, 'El formato del mensaje debe ser "Pedido NÚMERO_PEDIDO" Ejemplo: Pedido 1524. Por favor, asegúrate de incluir la palabra "Pedido" seguida de un espacio y el número de pedido.');
+        client.sendMessage(message.from, 'Porfavor eliga una alternativa:\n*. Para buscar por Número de Pedido, envía "Pedido NUMEROPEDIDO"\n*. Para buscar por Número de Documento, envía "Documento NUMERODOCUMENTO"');
     }
 });
 
